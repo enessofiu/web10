3aac5007d4765d9897494b8492e88e8becb41b1d
enessofiu <enessofiu04@gmail.com>
2024-05-25 13:42:29 +0200

    Update app.py

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -329,9 +329,12 @@ def main():
     st.pyplot(fig)
 
 
-# Footer
-st.markdown("<footer>Smart Agriculture Dashboard ©️ 2024</footer>", unsafe_allow_html=True)
 
 # Run the main function
 if __name__ == '__main__':
     main()
+
+
+
+# Footer
+st.markdown("<footer>Smart Agriculture Dashboard ©️ 2024</footer>", unsafe_allow_html=True)
