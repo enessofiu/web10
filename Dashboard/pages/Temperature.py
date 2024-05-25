5d9f09a434a4f3cf3118f693d6c7c2a23761307b
enessofiu <enessofiu04@gmail.com>
2024-05-25 13:21:30 +0200

    Update Temperature.py

--- Dashboard/pages/Temperature.py
+++ Dashboard/pages/Temperature.py
@@ -94,11 +94,11 @@ if col4.button("Show Scatter Plot"):
 # Display the corresponding chart based on the current chart type
 if st.session_state.current_chart == 'line':
     st.markdown("<div class='card1'><h3>Temperature Over Time</h3></div>", unsafe_allow_html=True)
-    st.line_chart(temp_data.set_index('timestamp')['TC'],color='#365341')
+    st.line_chart(temp_data.set_index('timestamp')['TC'],color='#77b5fe')
 
 elif st.session_state.current_chart == 'bar':
     st.markdown("<div class='card1'><h3>Temperature Distribution</h3></div>", unsafe_allow_html=True)
-    st.bar_chart(temp_data.set_index('timestamp')['TC'],color='#365341')
+    st.bar_chart(temp_data.set_index('timestamp')['TC'],color='#77b5fe')
 
 elif st.session_state.current_chart == 'pie':
     st.markdown("<div class='card1'><h3>Temperature Proportions</h3></div>", unsafe_allow_html=True)
